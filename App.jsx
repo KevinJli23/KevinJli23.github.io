ddcc298541b96b50343d41e613c178a8f08e1fa3
Kevin <107275055+KevinJli23@users.noreply.github.com>
2025-02-06 16:35:21 -0800

    testing design

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PortfolioHomepage from "./components/PortfolioHomepage";
+
+function App() {
+  return (
+    <div>
+      <PortfolioHomepage />
+    </div>
+  );
+}
+
+export default App;
